b933732e014244ef8555fda84c082a217d4fd92f
Daniel Colman <danRcolman@gmail.com>
2024-04-01 10:21:11 -0400

    Initial Commit

---
+++ DVTAssertionErrorDemo/DVTAssertionErrorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVTAssertionErrorDemoApp.swift
+//  DVTAssertionErrorDemo
+//
+//  Created by Daniel Colman on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DVTAssertionErrorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
